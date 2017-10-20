88a0764fd8761560394b892d857ab980b8a112c8
Ben Brewer <ben@benbrewer.me>
2017-10-19 19:40:19 -0500

    simplify the stubserver

--- lib/rspec/bash/stub_server.rb
+++ lib/rspec/bash/stub_server.rb
@@ -7,21 +7,24 @@ module Rspec
         @call_log_manager = call_log_manager
         @call_conf_manager = call_conf_manager
       end
-      def start
-        @tcp_server = TCPServer.new('localhost', 0)
-        @tcp_server.addr[1]
+      def start(tcp_server)
+        Thread.new do
+          accept_loop(tcp_server)
+        end
       end
-      def accept
-        tcp_socket = @tcp_server.accept
-
+      def accept_loop(tcp_server, loop_forever = true)
+        loop do
+          tcp_socket = tcp_server.accept
+          message = accept(tcp_socket)
+          tcp_socket.write(message)
+          tcp_socket.close
+          break unless loop_forever
+        end
+      end
+      def accept(tcp_socket)
         client_message = Marshal.load(tcp_socket)
         server_message = process_stub_call(client_message)
-
-        tcp_socket.write(Marshal.dump(server_message))
-        tcp_socket.close
-      end
-      def stop
-        @tcp_server.close
+        Marshal.dump(server_message)
       end
       def process_stub_call(stub_call)
         @call_log_manager.add_log(
--- spec/classes/stub_server_spec.rb
+++ spec/classes/stub_server_spec.rb
@@ -5,68 +5,66 @@ include Rspec::Bash
 describe 'StubServer' do
   let(:call_log_manager) {double(CallLogManager)}
   let(:call_conf_manager) {double(CallConfigurationManager)}
+  let(:tcp_server) { double(TCPServer) }
+  let(:accept_thread) { double(Thread) }
   subject {StubServer.new(call_log_manager, call_conf_manager)}
 
   context('#start') do
-    it('returns the random dynamic port that it is listening on') do
-      tcp_server = double(TCPServer)
-      allow(tcp_server).to receive(:addr)
-        .and_return(['AF_INET', 12345])
-      expect(TCPServer).to receive(:new)
-        .with('localhost', 0)
-        .and_return(tcp_server)
-      expect(subject.start).to eql(12345)
+    before do
+      allow(accept_thread).to receive(:kill)
+      allow(subject).to receive(:accept_loop)
+      allow(Thread).to receive(:new)
+        .and_yield
+        .and_return(accept_thread)
+    end
+    it('returns the thread that the server is accepting connections on') do
+      expect(subject.start(tcp_server)).to eql(accept_thread)
+    end
+    it('enters the accept loop') do
+      expect(subject).to receive(:accept_loop)
+        .with(tcp_server)
+      subject.start(tcp_server)
     end
   end
-  context('#start and #accept') do
-    it('accepts connections on the socket from start') do
-      client_message = {
+  context('#accept_loop') do
+    it('accepts the connection and replies') do
+      tcp_server = double(TCPServer)
+      tcp_socket = double(TCPSocket)
+      allow(tcp_server).to receive(:accept)
+        .and_return(tcp_socket)
+      allow(subject).to receive(:accept)
+        .with(tcp_socket)
+        .and_return('accept message')
+
+      expect(tcp_socket).to receive(:write)
+        .with('accept message')
+      expect(tcp_socket).to receive(:close)
+
+      subject.accept_loop(tcp_server, false)
+    end
+  end
+  context('#accept') do
+    it('correctly un/marshals data from a socket') do
+      call_from_client = {
         command: 'first_command',
         stdin:   'stdin',
         args:    %w(first_argument second_argument)
       }
-      server_message = {
+      conf_from_server = {
         args:     %w(first_argument second_argument),
         exitcode: 0,
         outputs:  []
       }
-      tcp_server = double(TCPServer)
+
+      allow(subject).to receive(:process_stub_call)
+        .with(call_from_client)
+        .and_return(conf_from_server)
+
       tcp_socket = double(TCPSocket)
-      expect(call_log_manager).to receive(:add_log)
-        .with('first_command', 'stdin', %w(first_argument second_argument))
-      expect(call_conf_manager).to receive(:get_best_call_conf)
-        .with('first_command', %w(first_argument second_argument))
-        .and_return(server_message)
-      allow(tcp_server).to receive(:addr)
-        .and_return(['AF_INET', 12345])
-      allow(TCPServer).to receive(:new)
-        .with('localhost', 0)
-        .and_return(tcp_server)
-      expect(tcp_server).to receive(:accept)
-        .and_return(tcp_socket)
       allow(tcp_socket).to receive(:to_str)
-        .and_return(Marshal.dump(client_message))
-      expect(tcp_socket).to receive(:write)
-        .with(Marshal.dump(server_message))
-      expect(tcp_socket).to receive(:close)
-
-      subject.start
-      subject.accept
-
-    end
-  end
-  context('#start and #stop') do
-    it('closes the socket it started') do
-      tcp_server = double(TCPServer)
-      allow(tcp_server).to receive(:addr)
-        .and_return(['AF_INET', 12345])
-      allow(TCPServer).to receive(:new)
-        .with('localhost', 0)
-        .and_return(tcp_server)
-      expect(tcp_server).to receive(:close)
-
-      subject.start
-      subject.stop
+        .and_return(Marshal.dump(call_from_client))
+      expect(subject.accept(tcp_socket))
+        .to eql(Marshal.dump(conf_from_server))
     end
   end
   context('#process_stub_call') do
