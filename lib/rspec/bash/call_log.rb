438b7196cd24de1ab8166dec65cb303b22505889
Ben Brewer <ben@benbrewer.me>
2017-10-26 16:08:30 -0400

    make call log work without a file

--- lib/rspec/bash/call_log.rb
+++ lib/rspec/bash/call_log.rb
@@ -1,12 +1,10 @@
 module Rspec
   module Bash
     class CallLog
-      def initialize(call_log_path = '')
-        @call_log_path = call_log_path
-      end
+      attr_accessor :call_log
 
-      def exist?
-        @call_log_path.exist?
+      def initialize(call_log_path = '')
+        @call_log = []
       end
 
       def stdin_for_args(*argument_list)
@@ -26,45 +24,24 @@ module Rspec
       end
 
       def called_with_no_args?
-        return false if call_log.empty?
+        return false if @call_log.empty?
 
-        call_log.all? do |call_log|
+        @call_log.all? do |call_log|
           argument_list = call_log[:args] || []
           argument_list.empty?
         end
       end
 
       def add_log(stdin, argument_list)
-        updated_log = call_log
+        updated_log = @call_log
         updated_log << {
           args: argument_list,
           stdin: stdin
         }
-        write updated_log
-      end
-
-      def call_log
-        @call_log_path.open('r') do |call_log|
-          YAML.load(call_log.read) || []
-        end
-      rescue NoMethodError, Errno::ENOENT
-        return []
-      end
-
-      def call_log=(new_log)
-        write new_log
       end
 
       def call_log_arguments
-        call_log.map { |call_log| call_log[:args] || [] }.compact
-      end
-
-      private
-
-      def write(call_log_to_write)
-        @call_log_path.open('w') do |call_log|
-          call_log.write call_log_to_write.to_yaml
-        end
+        @call_log.map { |call_log| call_log[:args] || [] }.compact
       end
     end
   end
